2b92b700ac2000be6f5f6046860e87ed18e74190
MathiasToscano02 <m.toscano23@studenti.unisa.it>
2026-05-17 19:31:13 +0200

    Completato sottomenù prenotazioni (case 4) e aggiunti commenti

--- src/main.c
+++ src/main.c
@@ -1,12 +1,24 @@
+/*
+*File: main.c
+*Autore: Mathias
+*Data: Maggio 2026
+*
+*Descrizione: Programma principale (Interfaccia Utente) per la gestione
+*dell'aula studio. Offre menù principale per gli ingressi diretti e un
+*sottomenù dedicato alla gestione completa delle prenotazioni a fasce orarie.
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "aula.h"
 #include "studente.h"
+#include "prenotazione.h"
 
 
 int main(){
-	//creo l'aula con 5 posti per fare i test
-	Aula mia_aula = aula_crea("Aula Studio Archimede", 5);
+	//creo l'aula con 50 posti per simulare il carico reale di un'aula studio universitaria
+	Aula mia_aula = aula_crea("Aula Studio Archimede", 50);
 	  if(mia_aula == NULL){
 		printf("Errore: impossibile creare l'aula.\n");
 		return -1;
@@ -22,6 +34,7 @@ int main(){
 	   printf("0. Esci\n");
 	   printf("Scegli un'opzione: ");
 
+		// La validazione del ritorno di scanf previene loop infiniti se l'utente inserisce caratteri non numerici
 		if(scanf("%d", &scelta) !=1){
 			while(getchar() != '\n'); //pulisce il buffer se l'utente inserisce una lettera
 			scelta = -1;
@@ -97,9 +110,105 @@ int main(){
 
 
 
-		case 4:
-			 printf("Funzionalità in sviluppo\n");
+		case 4: {
+			 int scelta_prenotazione;
+			 do{
+			     printf("\n=== SOTTOMENU GESTIONE PRENOTAZIONI ===\n");
+			     printf("1. Inserisci nuova prenotazione\n");
+			     printf("2. Effettua Check-in studente prenotato\n");
+			     printf("3. Annulla prenotazione esistente\n");
+			     printf("4. Stampa report statistico e storico\n");
+			     printf("0. Torna al menu principale\n");
+			     printf("Scegli un'opzione: ");
+
+				if(scanf("%d", &scelta_prenotazione) != 1) {
+					 while (getchar() != '\n');
+					 scelta_prenotazione = -1;
+					}
+
+				switch(scelta_prenotazione){
+					case 1: {
+					     char mat_pren[20], nome_pren[50], corso_pren[50];
+					     char data_pren[15], fascia_pren[30];
+
+					     printf("\n--- NUOVA PRENOTAZIONE ---\n");
+					     printf("Inserisci matricola: ");
+					     scanf("%s", mat_pren);
+					     printf("Inserisci nome: ");
+					     scanf("%s", nome_pren);
+					     printf("Inserisci corso: ");
+					     scanf("%s", corso_pren);
+					     printf("Inserisci data (GG/MM/AAAA): ");
+					     scanf("%s", data_pren);
+					     printf("Inserisci fascia oraria (es. 09:00-11:00): ");
+					     scanf("%s", fascia_pren);
+
+
+					     Studente stud = studente_crea(mat_pren, nome_pren, corso_pren);
+					     Prenotazione pren = prenotazione_crea(stud, data_pren, fascia_pren);
+
+						// La condizione verifica contemporaneamente la corretta allocazione e l'avvenuto inserimento nel registro
+						if(pren != NULL && aula_aggiungi_prenotazione(mia_aula, pren)) {
+							printf(">>> SUCCESSO: Prenotazione registrata in stato 'IN ATTESA'.\n");
+						}else {
+							printf(">>> ERRORE: Impossibile registrare la prenotazione.\n");
+							}
+						 break;
+						}
+
+					case 2: {
+					     char mat_checkin[20];
+					     printf("\n--- EFFETTUA CHECK-IN ---\n");
+					     printf("Inserisci la matricola dello studente: ");
+					     scanf("%s", mat_checkin);
+
+					     int risultato = aula_checkin_prenotazione(mia_aula, mat_checkin);
+						if(risultato == 1){
+							printf(">>> CHECK-IN OK: Studente seduto correttamente.\n");
+						}else if(risultato == 2){
+							printf(">>> CHECK-IN OK:Aula satura, studente in coda d'attesa.\n");
+							}else if(risultato == -1){
+							   printf(">>> AVVISO: Check-in già effettutato.\n");
+								}else {
+								   printf(">>> ERRORE: Nessuna prenotazione pendente trovata.\n");
+									}
+
+						 break;
+						}
+
+					case 3: {
+					     char mat_annulla[20];
+					     printf("\n--- ANNULLA PRENOTAZIONE ---\n");
+					     printf("Inserisci la matricola da annullare: ");
+					     scanf("%s", mat_annulla);
+
+						// Sfrutta la valutazione booleana del C: procede solo se la funzione restituisce un valore diverso da 0
+						if(aula_annulla_prenotazione(mia_aula, mat_annulla)){
+							printf(">>> SUCCESSO: Prenotazione annullata.\n");
+							}else{
+							  printf(">>> ERRORE: Impossibile annullare (inesistente o studente già entrato).\n");
+							  }
+						 break;
+						}
+
+					case 4:
+					     aula_stampa_report_prenotazioni(mia_aula);
+						 break;
+
+					case 0:
+					     printf("Ritorno al menu principale...\n");
+						 break;
+
+					default:
+					     printf("Opzione non valida\n");
+						 break;
+						}
+
+
+				}while(scelta_prenotazione != 0);
+
 			 break;
+			}
 
 
 		case 0:
@@ -120,7 +229,9 @@ int main(){
 
 
 	 }while (scelta !=0);
-
+	// Deallocazione profonda necessaria per prevenire memory leak prima della terminazione del processo
+	aula_distruggi(&mia_aula);
+	printf("Memoria ripulita correttamente. Arrivederci\n");
 
  return 0;
 
